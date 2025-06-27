e2140f4b3375d412de84c47a89ca85c6bfbfd44c
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-06-27 19:19:02 +0500

    done

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
+  img: String,
+  title: String,
+  category: String,
   description: String,
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+  author: {
+    name: String,
+    position: String,
+    avatar: String
   },
   ball: String,
   date: String
