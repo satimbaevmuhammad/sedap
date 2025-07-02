4b968c500efd14a9849ad93dd438b3b7b1bb6a9a
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-07-02 19:27:22 +0500

    done

--- models/Order.js
+++ models/Order.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
 
+const orderItemSchema = new mongoose.Schema({
+  img: String,
+  quanlity: Number,
+  price: Number,
+  totalPrice: Number,
+  menuSections: String,
+  productName: String,
+  reviews: Number,
+  rating: Number
+}, { _id: false });
+
+const deliverSchema = new mongoose.Schema({
+  deliverName: String,
+  deliverID: Number,
+  deliverNumber: String,
+  deliverTime: String
+}, { _id: false });
+
 const orderSchema = new mongoose.Schema({
-  id: String,
-  date: String, // format: "26 March 2020, 12:42 AM"
-  customerName: String,
-  location: String,
-  amount: Number,
-  status: String,
-  category: String
+  orderID: String, // "#5552351"
+  date: String, // "26 March 2020, 12:42 AM"
+  customerName: String, // "Mikasa Ackerman"
+  location: String, // "Corner Street 5th London"
+  totalPrice: Number, // 164.52
+  statusOrder: String, // "New Order"
+  userSatus: String, // "Customer"
+  orderItems: [orderItemSchema],
+  deliver: deliverSchema
 });
 
 module.exports = mongoose.model('Order', orderSchema);
